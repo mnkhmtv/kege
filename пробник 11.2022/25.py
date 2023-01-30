ce0622d01efa990904284b0956a76e455a7fb512
mnkhmtv <mnkhmtvv@gmail.com>
2023-01-30 18:32:25 +0300

    the first commit

--- пробник 11.2022/25.py
+++ пробник 11.2022/25.py
@@ -7,13 +7,11 @@ for i1 in a:
     for i2 in s:
         delit = []
         res = int('3' + str(i1) + '52' + i2)
-
-        for i in range(2,int(res**0.5)+1):
-            if res%i==0:
-                delit+=[i]
-                delit.sort()
-
-        if len(set(delit))%2!=0:
-            need+=[[res, max(delit)]]
-            
+        if res/int(res**0.5)==res**0.5:
+            for i in range(2,int(res**0.5)+1):
+                if res%i==0:
+                    delit+=[res//i]
+            need+=[[res,max(delit)]]
+need.sort()
 print(need)
+
